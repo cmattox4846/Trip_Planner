151d97c8e61e7d2d8a45a6a2a16127b0d1743fac
cmattox4846 <cmattox4846@gmail.com>
2021-09-24 16:30:16 -0400

    Added individual confirmations to each portion of trip

--- Trip.js
+++ Trip.js
@@ -9,17 +9,104 @@ let desResult;
 let resResult;
 let transResult;
 let enterResult;
+let desChoiceResults;
+let foodChoiceResults;
+
+
+function getRandomTrip(){
+    desResult = destinationGen(destination);
+    resResult = resturantGen(resturant);
+    transResult = transportationGen(transport);
+    enterResult = entertainmentGen(entertainment);
+}
+function randomSelection(){
+    getRandomTrip()
+    let result = window.confirm('Here is your random vacation options!'+ '\n' + 'Your destination will be: ' + desResult +'\n' + 'Your dining choice will be: ' + resResult +'\n'+ 'You will travel by: ' + transResult +'\n'+ 'While you are there you will enjoy a: ' + enterResult +'\n'+'Please choose OK your confirm or Cancel to choose again!');
+    
+    if (result === true){
+        console.log(result)
+        return true 
+    }
+    else {
+        console.log(result)
+        return false
+        
+    }
+
+}
+
+
+function chooseYes(selectionString){
+    let choiceResultTrue = true
+    let choiceResultFalse = false
+    
+    let result = window.confirm('You selection is '+ selectionString + '\n' + 'Enter Yes to confirm No to choose');
+    
+    if (result === true){
+        console.log(result)
+        return choiceResultTrue
+    }
+    else {
+        console.log(result)
+        return choiceResultFalse
+        
+    }
+    
+}   
+    
+   
+let confirmOrNot = randomSelection()
+
+
+
+
+
 
 //fuctions to random generate a selection
 
 function destinationGen(local){
-    let selected = local[Math.floor(Math.random()*local.length)];
-    return selected
+    let successful = false
+    let selected
+    selected = local[Math.floor(Math.random()*local.length)];
+    desChoiceResults = chooseYes(selected);
+    while (successful === false ){
+       
+    
+    
+    if (desChoiceResults === true){
+        successful = true
+        return selected
+    }
+    else{
+        selected = local[Math.floor(Math.random()*local.length)];
+        desChoiceResults = chooseYes(selected)
+    }
+        
+    }
 };
 
+
 function resturantGen(food){
-    let selected = food[Math.floor(Math.random()*food.length)];
-    return selected
+
+    let successful = false
+    let selected
+    selected = food[Math.floor(Math.random()*food.length)];
+    foodChoiceResults = chooseYes(selected)
+    while (successful === false ){
+       
+    
+    
+    if (foodChoiceResults === true){
+        successful = true
+        return selected
+    }
+    else{
+        selected = food[Math.floor(Math.random()*food.length)];
+        foodChoiceResults = chooseYes(selected)
+    }
+        
+    }
+    
 };
 
 function transportationGen(wayAround){
@@ -31,6 +118,11 @@ function entertainmentGen(enterntainment){
     let selected = entertainment[Math.floor(Math.random()*entertainment.length)];
     return selected
 };
+
+
+
+
+
 
 
 //function that combines selections and displays comfirmation box
@@ -51,14 +143,6 @@ function randomSelection(){
 
 }
 
-function getRandomTrip(){
-    desResult = destinationGen(destination);
-    resResult = resturantGen(resturant);
-    transResult = transportationGen(transport);
-    enterResult = entertainmentGen(entertainment);
-}
-
-let confirmOrNot = randomSelection()
 
 //function that will loop until user has confirmed they are happy with results
 
